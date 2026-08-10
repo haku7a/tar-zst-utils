5e08cddb4455fa77f63e8684d41a489d1cb4fc5e
haku7a <v.a.neyaskin@gmail.com>
2026-08-10 14:27:30 +0300

    feat: add zstd decompression

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 from pathlib import Path
 
+import zstandard as zstd
+
 parser = argparse.ArgumentParser()
 parser.add_argument("file")
 args = parser.parse_args()
@@ -8,4 +10,10 @@ args = parser.parse_args()
 
 path = Path(args.file)
 
-print(path.resolve())
+out_tar_path = path.with_suffix("")
+
+
+with open(path, "rb") as f:
+    decompressor = zstd.ZstdDecompressor()
+    with open(out_tar_path, "wb") as out:
+        decompressor.copy_stream(f, out)
