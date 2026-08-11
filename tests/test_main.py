54e71a3d43b9bba674cccf1956764af3a34f76d5
haku7a <v.a.neyaskin@gmail.com>
2026-08-12 02:49:36 +0300

    refactor: fix test name type

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,7 @@ import pytest
 from main import main
 
 
-def test_a(monkeypatch, capsys):
+def test_missing_required_flag_raises_system_exit(monkeypatch, capsys):
 
     monkeypatch.setattr(sys, "argv", ["main.py", "arch-wiki-docs.pkg.tar.zst"])
 
