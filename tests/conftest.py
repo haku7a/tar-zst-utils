fe212d3a1a9345d5cef8fced813629548a1978ee
haku7a <v.a.neyaskin@gmail.com>
2026-08-17 22:41:28 +0300

    tests: add fixture to create tar archive

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+import tarfile
+from pathlib import Path
+
 import pytest
 
 
@@ -6,3 +9,13 @@ def text_file(tmp_path):
     file_path = tmp_path / "sample.txt"
     file_path.write_text("sample text")
     return file_path
+
+
+@pytest.fixture
+def tar_file(text_file: Path) -> Path:
+
+    output_path = text_file.parent / "tar_file.tar"
+    with tarfile.open(output_path, mode="w") as tar:
+        tar.add(text_file, arcname=text_file.name)
+
+    return output_path
