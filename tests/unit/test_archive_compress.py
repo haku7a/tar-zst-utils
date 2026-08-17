ae6fe9e120b4e4d2b533599e3d986d79668aa037
haku7a <v.a.neyaskin@gmail.com>
2026-08-17 03:04:25 +0300

    test: extract make_args helper

--- tests/unit/test_archive_compress.py
+++ tests/unit/test_archive_compress.py
@@ -3,8 +3,17 @@ import argparse
 from commands.archive_compress import archive_compress
 
 
+def _make_args(file, output=None, level=3, threads=0):
+    return argparse.Namespace(
+        file=str(file),
+        output=output,
+        level=level,
+        threads=threads,
+    )
+
+
 def test_creates_non_empty_tar_zst_archive(text_file):
-    args = argparse.Namespace(file=str(text_file), level=15, threads=0, output=None)
+    args = _make_args(text_file)
     archive_compress(args)
     expected = text_file.with_name(text_file.name + ".tar.zst")
 
